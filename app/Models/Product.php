8d1511f48d8f920f6ceb94fe510958ce2da91982
Bee <bee@iMac-de-Miguel-Varandas.local>
2025-07-14 18:58:02 +0100

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+ public function user (){
+     return $this->belongsTo(related: User::class, foreignKey: "user_id");
+ }
+ public function category (){
+     return $this->belongsTo(related: Category::class, foreignKey: 'category_id');
+ }
+}
